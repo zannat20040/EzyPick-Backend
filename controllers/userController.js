c8d3de648c25989adde40f44fdbb0bfd83b4ba22
zannat20040 <shanzidadisha1234@gmail.com>
2025-04-22 22:30:45 +0600

    get user by email

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,30 +64,27 @@ const registerUser = async (req, res) => {
   }
 };
 
-// login controller
-// const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
+const getUserByEmail = async (req, res) => {
+  const { email } = req.params;
 
-//   try {
-//     const user = await User.findOne({ email });
+  if (!email) {
+    return res.status(400).json({ message: "Email is required." });
+  }
 
-//     if (!user) {
-//       return res.status(400).json({ error: "User not found." });
-//     }
+  try {
+    const user = await User.findOne({ email });
 
-//     // Compare entered password with stored hash
-//     const isMatch = await bcryptjs.compare(password, user.password);
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
 
-//     if (!isMatch) {
-//       return res.status(400).json({ error: "Invalid credentials." });
-//     }
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch user", error: err.message });
+  }
+};
 
-//     res.status(200).json({ message: "Login successful!", user });
-//   } catch (err) {
-//     res
-//       .status(500)
-//       .json({ error: "Something went wrong. Please try again later." });
-//   }
-// };
-
-module.exports = { registerUser };
+module.exports = {
+  registerUser,
+  getUserByEmail, 
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userController");
+const {
+  registerUser,
+  getUserByEmail,
+} = require("../controllers/userController");
 
 router.post("/register", registerUser);
-// router.post("/login", loginUser);
+router.get("/:email", getUserByEmail);
 
 module.exports = router;
